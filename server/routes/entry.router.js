7b9b24ddb5e7d638957a2838e1fbd4a36bd47dcb
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-05-11 15:38:40 -0500

    started sort by duration

--- server/routes/entry.router.js
+++ server/routes/entry.router.js
@@ -6,32 +6,41 @@ const pool = require('../modules/pool');
 // Routes
 
 // POST route
-router.post('/', (req,res) => {
+router.post('/', (req, res) => {
     console.log('initiate POST query');
     const newEntry = req.body;
     const queryText = `INSERT INTO "entry" ("line_item", "description", "project", "date", "hours")
     VALUES ($1, $2, $3, $4, $5);`;
     pool.query(queryText, [newEntry.lineItem, newEntry.description, newEntry.project, newEntry.date, newEntry.billingHours])
-    .then((result) => {
-        res.sendStatus(201);
-    }).catch((error) => {
-        console.log('Error making query', error);
-        res.sendStatus(500);
-    });
+        .then((result) => {
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log('Error making query', error);
+            res.sendStatus(500);
+        });
 });// end POST route
 
 
 // GET route
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     console.log('initiate GET query');
-    const queryText = `SELECT "id", "line_item", "description", "project", "date", "hours" FROM "entry";`;
-    pool.query(queryText)
-    .then((result) => {
-        res.send(result.rows);
-    }).catch((error) => {
-        console.log('Error making query', error);
-        res.sendStatus(500);
-    });
+    if (req.body.sort = "hoursDesc") {
+        const queryText = `SELECT "id", "line_item", "description", "project", "date", "hours" FROM "entry" ORDER BY "hours" DESC;`;
+        doTheGet(queryText);
+    }
+    else {
+        const queryText = `SELECT "id", "line_item", "description", "project", "date", "hours" FROM "entry" ORDER BY "line_item"`;
+        doTheGet(queryText);
+    }
+    function doTheGet(queryText) {
+        pool.query(queryText)
+            .then((result) => {
+                res.send(result.rows);
+            }).catch((error) => {
+                console.log('Error making query', error);
+                res.sendStatus(500);
+            });
+    }
 });
 // end GET route
 
