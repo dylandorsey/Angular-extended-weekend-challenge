be8ebdaf21ee1b7e6941b62860de19c240d1f32c
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-05-13 14:29:27 -0500

    update project

--- server/routes/project.router.js
+++ server/routes/project.router.js
@@ -5,21 +5,21 @@ const pool = require('../modules/pool');
 
 // Routes
 
-// POST route
-router.post('/', (req,res) => {
-    console.log('initiate POST query');
-    const newProject = req.body;
-    const queryText = `INSERT INTO "project" ("name")
-    VALUES ($1);`;
-    pool.query(queryText, [newProject.name])
-    .then((result) => {
-        res.sendStatus(201);
-    }).catch((error) => {
-        console.log('Error making query', error);
-        res.sendStatus(500);
-    });
-}); // end POST route
-
+// DELETE route
+router.delete('/:id', (req, res) => {
+    const entry_id = req.params.id;
+    console.log(entry_id);
+    pool.query(`DELETE FROM "project"
+                        WHERE "id" = ($1);`, [entry_id])
+        .then((results) => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log('problem with DELETE from database', error);
+            res.sendStatus(500);
+        });
+});
+// end DELETE route
 
 // GET route
 router.get('/', (req,res) => {
@@ -38,24 +38,39 @@ router.get('/', (req,res) => {
 });
 // end GET route
 
+// POST route
+router.post('/', (req,res) => {
+    console.log('initiate POST query');
+    const newProject = req.body;
+    const queryText = `INSERT INTO "project" ("name")
+    VALUES ($1);`;
+    pool.query(queryText, [newProject.name])
+    .then((result) => {
+        res.sendStatus(201);
+    }).catch((error) => {
+        console.log('Error making query', error);
+        res.sendStatus(500);
+    });
+}); 
+// end POST route
+
 // PUT route
+router.put('/', (req,res) => {
+    console.log('initiate POST query');
+    const project = req.body;
+    const queryText = `UPDATE "project" 
+    SET "name" = ($1)
+    WHERE "id" = ($2);`;
+    pool.query(queryText, [project.name, project.id])
+    .then((result) => {
+        res.sendStatus(201);
+    }).catch((error) => {
+        console.log('Error making query', error);
+        res.sendStatus(500);
+    });
+}); 
+// end PUT route
 
-// end GET route
 
-// DELETE route
-router.delete('/:id', (req, res) => {
-    const entry_id = req.params.id;
-    console.log(entry_id);
-    pool.query(`DELETE FROM "project"
-                        WHERE "id" = ($1);`, [entry_id])
-        .then((results) => {
-            res.sendStatus(200);
-        })
-        .catch((error) => {
-            console.log('problem with DELETE from database', error);
-            res.sendStatus(500);
-        });
-});
-// end DELETE route
 
 module.exports = router;
